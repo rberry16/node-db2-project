d66fd423117c66a4d7938adcbd5f890a383e8684
Ryan Berry <rberry16@hotmail.com>
2022-06-14 09:40:40 -0700

    cars seed built

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,32 @@
 // STRETCH
+const cars = [
+    {
+        vin: 'ZAMGJ45A480037578',
+        make: 'Ford',
+        model: 'Focus',
+        mileage: 21000,
+        title: 'clean',
+        transmission: 'automatic'
+    },
+
+    {
+        vin: 'JH4KA4660KC003974',
+        make: 'Dodge',
+        model: 'Charger',
+        mileage: 9000,
+        title: 'salvage',
+        transmission: 'manual'
+    },
+
+    {
+        vin: '1HD1PDC392Y952267',
+        make: 'Kia',
+        model: 'Spectra',
+        mileage: 192000
+    }
+];
+
+exports.seed = async function (knex) {
+    await knex('cars').truncate();
+    await knex('cars').insert(cars);
+};
